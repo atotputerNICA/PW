b7d0cb4d39d31d65d2a27fbba251d37c805a499f
atotputerNICA <nicadanamaria@gmail.com>
2019-03-18 00:35:58 +0200

    Solved Json Bug

--- web/src/js/select.js
+++ web/src/js/select.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  $('#select-country').niceSelect();
-  $("#select-language").niceSelect();
-  $('#select-country').empty();
 
+  $('#select-country').empty();
   $('#select-country').append('<option selected="true" disabled>Choose Country</option>');
-  // $('#select-country').prop('selectedIndex', 0);
+  $('#select-country').prop('selectedIndex', 0);
 
   // Populate dropdown with list of provinces
   // console.log(countries);
   countries.forEach(function (entry, key) {
-      console.log(key);
+      // console.log(key);
       $('#select-country').append($('<option></option>').val(entry.code).text(entry.name));
-  })
+  });
   // $('#select-country').change();
-  console.log( $('#select-country').html());
-
+  // console.log( $('#select-country').html());
+  $('#select-country').niceSelect();
+  $("#select-language").niceSelect();
+  $('#select-country').niceScroll();
 });
